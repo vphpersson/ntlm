d4a9f5884ba04d202e5c290cd0670143cbcfd093
Viktor Persson <vph.persson@gmail.com>
2020-01-21 21:25:14 +0100

    Restored crypto dependency.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'pycryptodome @ https://github.com/Legrandin/pycryptodome/targball/master',
+        'pycryptodome',
         'msdsalgs @ https://github.com/vphpersson/msdsalgs/tarball/master'
     ]
 )
