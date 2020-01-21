f370804fcb7cc94d7a254b863a23335de87db77a
Viktor Persson <vph.persson@gmail.com>
2020-01-21 22:37:57 +0100

    Restructured.

--- ntlm/messages/__init__.py
+++ ntlm/messages/__init__.py
@@ -1,36 +1,54 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import ClassVar, Optional, Type
+from typing import ClassVar, Type, Dict
 from abc import ABC, abstractmethod
+from struct import unpack as struct_unpack
 
-from ntlm.exceptions import MalformedNTLMSignatureError, UnexpectedMessageTypeError, \
-    MalformedNTLMMessageError
+from ntlm.exceptions import MalformedSignatureError, UnexpectedMessageTypeError, \
+    MalformedMessageError
 
 
 @dataclass
-class NTLMMessage(ABC):
-    signature: ClassVar[bytes] = b'NTLMSSP\x00'
-    message_type_id: ClassVar[int] = NotImplemented
-    _malformed_exception_class: ClassVar[Type[MalformedNTLMMessageError]] = NotImplemented
+class Message(ABC):
+    SIGNATURE: ClassVar[bytes] = b'NTLMSSP\x00'
+    MESSAGE_TYPE_ID_TO_MESSAGE_CLASS: ClassVar[Dict[int, Type[Message]]] = {}
+
+    MESSAGE_TYPE_ID: ClassVar[int] = NotImplemented
+    MALFORMED_MESSAGE_ERROR_CLASS: ClassVar[Type[MalformedMessageError]] = NotImplementedError
 
     @classmethod
-    def check_signature(cls, signature_data: bytes) -> None:
-        if signature_data != cls.signature:
-            raise cls._malformed_exception_class from MalformedNTLMSignatureError(observed_signature=signature_data)
+    def from_bytes(cls, data: bytes) -> Message:
+
+        import ntlm.messages.negotiate
+        import ntlm.messages.challenge
+        import ntlm.messages.authenticate
+
+        signature_data: bytes = struct_unpack('<8s', data[0:8])[0]
+        if signature_data != cls.SIGNATURE:
+            raise MalformedSignatureError(observed_signature=signature_data)
+
+        message_type_id: int = struct_unpack('<I', data[8:12])[0]
+
+        if cls != Message:
+            if message_type_id != cls.MESSAGE_TYPE_ID:
+                raise UnexpectedMessageTypeError(
+                    observed_ntlm_message_type_id=message_type_id,
+                    expected_message_type_id=cls.MESSAGE_TYPE_ID
+                )
+            return cls._from_bytes(data=data)
+        else:
+            return cls.MESSAGE_TYPE_ID_TO_MESSAGE_CLASS[message_type_id].from_bytes(data=data)
 
     @classmethod
     @abstractmethod
-    def from_bytes(cls, message_bytes: bytes) -> NTLMMessage:
-        pass
+    def _from_bytes(cls, data: bytes) -> Message:
+        raise NotImplementedError
 
     @abstractmethod
     def __bytes__(self) -> bytes:
-        pass
+        raise NotImplementedError
 
-    @classmethod
-    def check_message_type(cls, message_type_id: int, message_bytes: Optional[bytes] = None) -> None:
-        if message_type_id != cls.message_type_id:
-            raise cls._malformed_exception_class(message_bytes=message_bytes) from UnexpectedMessageTypeError(
-                observed_ntlm_message_type_id=message_type_id,
-                expected_message_type_id=cls.message_type_id
-            )
+
+def register_ntlm_message(cls: Type[Message]):
+    cls.MESSAGE_TYPE_ID_TO_MESSAGE_CLASS[cls.MESSAGE_TYPE_ID] = cls
+    return cls
